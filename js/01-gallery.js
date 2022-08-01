6bcfa20a349a6a0a9dfe887c3d08f5e376e1e8b7
Andreii-Denysenko <a.denisenko2111@gmail.com>
2022-08-01 21:40:47 +0300

    solution task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const addMurkup = createImageMurkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", addMurkup);
+
+function createImageMurkup(galleryItems) {
+return galleryItems
+.map(({preview, original, description})=>{
+    return  `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+    </a>
+    </div>
+    ` 
+})
+.join("");
+};
+// вешаем слушатель собитий
+galleryContainer.addEventListener("click", onModal);
+
+function onModal(evt){
+    evt.preventDefault();
+
+    let instance = basicLightbox.create(`
+    <img src='${evt.target.dataset.source}' width="800" height="600">`,
+    {
+        onShow: (instance) => {
+          window.addEventListener("keydown", onCloseEsc);
+        },
+        onClose: (instance) => {
+          window.removeEventListener("keydown", onCloseEsc);
+        },
+      }
+    );
+    function onCloseEsc(evt) {
+      if (evt.code === "Escape") {
+        instance.close();
+      } else {
+        return;
+      }
+    }
+instance.show();
+return instance;
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const galleryContainer = document.querySelector('.gallery');
+const addMurkup = createImageMurkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", addMurkup);
+
+function createImageMurkup(galleryItems) {
+return galleryItems
+.map(({preview, original, description})=>{
+    return`
+    <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+})
+.join("");
+};
